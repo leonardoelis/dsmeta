3182023622b9314ddb22dcf85cb83ee2f6460dcd
leonardoelis <leonardo.elis@gmail.com>
2022-09-07 20:18:23 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    // <></> (fragment) = usado para retornar mais de uma tag
+    <> 
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import notificationIcon from "../../assets/img/notification-icon.svg"
+import "./styles.css"
+
+function NotificationButton() {
+    return (
+        // className é igual ao class do HTML. N pode usar o class, pois a palavra class é uma palavra reservada do javasrcipt
+        <div className="dsmeta-red-btn">
+            <img src={notificationIcon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
