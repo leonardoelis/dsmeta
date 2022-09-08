650fae05a8d55f8ea80de57b70aacddd6382da57
leonardoelis <leonardo.elis@gmail.com>
2022-09-08 18:07:46 -0300

    API test

---
+++ backend/src/main/java/com/leonardoelis/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.leonardoelis.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leonardoelis.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
