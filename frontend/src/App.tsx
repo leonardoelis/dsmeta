1edb959e147747f12dc0830ae9d5cf42a4204681
leonardoelis <leonardo.elis@gmail.com>
2022-09-11 02:35:19 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
@@ -5,6 +7,7 @@ function App() {
   return (
     // <></> (fragment) = usado para retornar mais de uma tag
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
